7c5c2e6e5fe3cb196c42dd0cbb965597f08eea08
Emanuele Simula <simulaemanuele@gmail.com>
2024-08-14 13:13:08 +0200

    fix: deleted all the unused files. Now only the deploy is available

--- types/types.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@empglabs/react-native-image-slider-box'
-declare module "*.png"
-declare module "*.jpg"
-declare module "react-native-star-view"
-declare module "dateformat"
-declare module "react-native-vector-icons/*"
-declare module "react-native-video-controls"
